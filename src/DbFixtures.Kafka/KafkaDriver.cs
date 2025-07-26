d0d68a5fa7e40493ae90aced9772fd4d8a238fd2
Pedro Henriques <pedro@pedrojhenriques.com>
2025-07-26 17:19:07 +0000

    Add initial structure for the Kafka driver.

---
+++ src/DbFixtures.Kafka/KafkaDriver.cs
@@ -0,0 +1,21 @@
+﻿using SharedLibs.Types;
+
+namespace DbFixtures.Kafka;
+
+public class KafkaDriver : IDriver
+{
+  public Task Close()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task InsertFixtures<T>(string tableName, T[] fixtures)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task Truncate(string[] tableNames)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ test/unit/DbFixtures.Kafka.Tests/KafkaDriver.cs
@@ -0,0 +1,12 @@
+namespace DbFixtures.Kafka.Tests.Unit;
+
+[Trait("Type", "Unit")]
+public class KafkaDriverTests : IDisposable
+{
+  public KafkaDriverTests() { }
+
+  public void Dispose()
+  {
+    throw new NotImplementedException();
+  }
+}
