58776d0ce5dd8263fc99982aa09b310ff451aa9f
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-02 16:35:02 +0000

    Add scheleton of the Redis driver.

---
+++ src/DbFixtures.Redis/RedisDriver.cs
@@ -0,0 +1,23 @@
+﻿using SharedLibs.Types;
+
+namespace DbFixtures.Redis;
+
+public class RedisDriver : IDriver
+{
+  public RedisDriver() { }
+
+  public Task Close()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task InsertFixtures<T>(string tableName, T[] fixtures)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task Truncate(string[] tableNames)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ test/unit/DbFixtures.Redis.Tests/RedisDriver.cs
@@ -0,0 +1,11 @@
+namespace DbFixtures.Redis.Tests.Unit;
+
+[Trait("Type", "Unit")]
+public class RedisDriver : IDisposable
+{
+  public RedisDriver() { }
+
+  public void Dispose()
+  {
+  }
+}
