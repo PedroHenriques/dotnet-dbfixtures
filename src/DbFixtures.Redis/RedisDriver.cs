f14e231e694a6905f3ac578871fdf85680e4cb1b
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-03 21:18:16 +0000

    Add implementation of the Redis driver.

--- src/DbFixtures.Redis/RedisDriver.cs
+++ src/DbFixtures.Redis/RedisDriver.cs
@@ -1,23 +1,96 @@
-﻿using SharedLibs.Types;
+﻿using DbFixtures.Redis.Types;
+using SharedLibs.Types;
+using StackExchange.Redis;
 
 namespace DbFixtures.Redis;
 
 public class RedisDriver : IDriver
 {
-  public RedisDriver() { }
+  private readonly IConnectionMultiplexer _client;
+  private readonly IDatabase _db;
+  private readonly Dictionary<string, KeyTypes> _keyTypes;
+
+  public RedisDriver(
+    IConnectionMultiplexer client, IDatabase db,
+    Dictionary<string, KeyTypes> keyTypes
+  )
+  {
+    this._client = client;
+    this._db = db;
+    this._keyTypes = keyTypes;
+  }
 
   public Task Close()
   {
-    throw new NotImplementedException();
+    this._client.Close();
+    return Task.CompletedTask;
   }
 
-  public Task InsertFixtures<T>(string tableName, T[] fixtures)
+  public async Task InsertFixtures<T>(string tableName, T[] fixtures)
   {
-    throw new NotImplementedException();
+    if (fixtures.Length == 0) { return; }
+
+    KeyTypes keyType;
+    if (this._keyTypes.TryGetValue(tableName, out keyType) == false)
+    {
+      throw new Exception($"The key '{tableName}' doesn't have a declared type, in the dictionary provided in the constructor of this driver");
+    }
+
+    switch (keyType)
+    {
+      case KeyTypes.String:
+        var result = await this._db.StringSetAsync(
+          tableName, new RedisValue(fixtures[0].ToString())
+        );
+        if (result == false)
+        {
+          throw new Exception($"Failed to insert string key '{tableName}'");
+        }
+        return;
+
+      case KeyTypes.List:
+        RedisValue[] listValues = Array.ConvertAll(
+          fixtures, fixture => new RedisValue(fixture.ToString())
+        );
+        await this._db.ListLeftPushAsync(tableName, listValues);
+        return;
+
+      case KeyTypes.Set:
+        RedisValue[] setValues = Array.ConvertAll(
+          fixtures, fixture => new RedisValue(fixture.ToString())
+        );
+        await this._db.SetAddAsync(tableName, setValues);
+        return;
+
+      case KeyTypes.Stream:
+        var streamFixture = (fixtures as Dictionary<string, string>[])[0];
+        NameValueEntry[] streamValues = streamFixture.Select(
+          pair => new NameValueEntry(pair.Key, pair.Value)
+        ).ToArray();
+
+        await this._db.StreamAddAsync(tableName, streamValues);
+        return;
+
+      case KeyTypes.Hash:
+        var hashFixture = (fixtures as Dictionary<string, string>[])[0];
+        HashEntry[] hashValues = hashFixture.Select(
+          pair => new HashEntry(pair.Key, pair.Value)
+        ).ToArray();
+
+        await this._db.HashSetAsync(new RedisKey(tableName), hashValues);
+        return;
+    }
   }
 
-  public Task Truncate(string[] tableNames)
+  public async Task Truncate(string[] tableNames)
   {
-    throw new NotImplementedException();
+    RedisKey[] keys = Array.ConvertAll(tableNames, name => (RedisKey)name);
+    var numDelKeys = await this._db.KeyDeleteAsync(keys);
+
+    var numReqKeys = tableNames.Length;
+    if (numDelKeys != numReqKeys)
+    {
+      throw new Exception($"Failed to delete all the requested keys. Deleted {numDelKeys} of {numReqKeys} keys.");
+    }
   }
 }
